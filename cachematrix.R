1ec7f29eed6b4e0a6037b632447bfc52d427590c
kzahra <kurtzahra87@gmail.com>
2015-05-23 14:56:23 +0300

    create functions
    create functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## this function is used to create a custom object
+## which has a matrix and its inverse. When this constructor
+## is used a 'cachable matrix', these are created in the R environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## this function checks whether the inverse of 
+## the matrix is available from cache. If so, it
+## returns the inverse matrix. If not, then
+## the inverse of the matrix is calculated and stored in 
+## the cache for next time and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
